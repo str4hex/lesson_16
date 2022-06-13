c27024654cdc884cb91c882c6d00035b349a667d
srt3for <evdookimov@mail.ru>
2022-06-13 23:54:16 +0300

    +add write base

--- write_db.py
+++ write_db.py
@@ -4,12 +4,13 @@ from app import app
 from models.user import User
 from models.offer import Offer
 from models.order import Order
+import datetime
 
 Users = "./json/Users.json"
 Orders = "./json/Orders.json"
 Offers = "./json/Offers.json"
 
-"""with open(Users, "r", encoding="utf-8") as json_user:
+with open(Users, "r", encoding="utf-8") as json_user:
     load_json = json.load(json_user)
     for loads in load_json:
         users = User(id=loads["id"], first_name=loads["first_name"], last_name=loads["last_name"], age=loads["age"],
@@ -17,14 +18,17 @@ Offers = "./json/Offers.json"
         with app.app_context():
             db.session.add(users)
             db.session.commit()
-    print(f'{json_user} - добавлено в СУБД')"""
+    print(f'{json_user} - добавлено в СУБД')
 
 with open(Orders, "r", encoding="utf-8") as json_user:
     load_json = json.load(json_user)
     for loads in load_json:
-        users = Order(id=loads["id"], name=loads["name"], description=loads["description"], start_date=["start_date"],
-                      end_date=loads["end_date"], address=loads["address"], price=loads["price"],
-                      customer_id=loads["customer_id"], executor_id=loads["executor_id"])
+        start_dates = loads['start_date'].split("/")
+        end_dates = loads['end_date'].split("/")
+        users = Order(id=loads["id"], name=loads["name"], description=loads["description"],
+                      start_date=datetime.date(int(start_dates[2]),int(start_dates[0]),int(start_dates[1])),
+                      end_date = datetime.date(int(end_dates[2]),int(end_dates[0]),int(end_dates[1])), address = loads["address"], price = loads["price"],
+                      customer_id = loads["customer_id"], executor_id = loads["executor_id"])
 
         with app.app_context():
             db.session.add(users)
@@ -35,7 +39,6 @@ with open(Offers, "r", encoding="utf-8") as json_user:
     load_json = json.load(json_user)
     for loads in load_json:
         users = Offer(id=loads["id"], order_id=loads["order_id"], executor_id=loads["executor_id"])
-
         with app.app_context():
             db.session.add(users)
             db.session.commit()
